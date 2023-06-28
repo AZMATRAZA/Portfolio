25a994f9e824866ceacf98c8bb75782f0ebb83d4
Md Azmat Raza <61646903+AZMATRAZA@users.noreply.github.com>
2023-06-28 23:25:55 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,166 @@
+const logotext = "MD AZMAT RAZA";
+const meta = {
+    title: "Md Azmat Raza",
+    description: "I’m Md Azamt Raza, a Software developer with 4 years of experience in web development. I have worked with many startups and established companies to build their websites. I love coding and always look for new challenges and opportunities to learn new things.",
+};
+
+const introdata = {
+    title: "I’m Md Azmat Raza",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop web apps",
+    },
+    description: "I’m Md Azamt Raza, a Software developer with 4 years of experience in web development. I have worked with many startups and established companies to build their websites. I love coding and always look for new challenges and opportunities to learn new things.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About my Self",
+    aboutme: "Have exposure♛ to building professional websites🖥 and front-end applications 💻. Experienced with a demonstrated history of working in the information technology and services industries🏢 . Skilled in FrontEnd Technologies✍️ as React.js, Redux, EcmaScript, JavaScript, HTML, CSS, Bootstrap, Flexbox, .GitHub, Net, SQL, and C# for web and window-based applications. Apart from my professional journey, I am a learner📝 and enthusiastic to learn new tech. I have created 15+ mini & major projects💼 on Reactjs and other technologies.Also deployed and having live-hosted projects📽, checkout out my GitHub pages📒 or YouTube📲 channel for more details. Responsibilities- Developing new user-facing features using React.js Building reusable components and front-end libraries for future use Translating designs into high quality code Optimizing components for maximum performance Optimizing performance of the application.Troubleshooting the bugs Skills- Strong proficiency in JavaScript and the JavaScript object model In-depth understanding of React.js and its core principles Experience with popular React.js workflows (Redux) Experience with React Hooks and building custom react-hooks Familiarity with newer specifications of ECMAScript Knowledge of server-side rendering Familiarity with RESTful APIs Knowledge of Typescript Knowledge of Redux middleware such as Redux Saga & Redux Thunk Good Understanding with common front-end development tools such as Babel, Webpack, NPM, yarn etc.",
+};
+const worktimeline = [{
+        jobtitle: "Software Developer",
+        where: "Foray Software Pvt Ltd",
+        date: "Feb 2022 - Oct 2022",
+    },
+    {
+        jobtitle: "Software Developer",
+        where: "Zawsoft web solutions",
+        date: "Aug 2018 - Jan 2022",
+    },
+    {
+        jobtitle: "HCL CDC",
+        where: "Intern",
+        date: "Dec 2014 - May 2015",
+    },
+];
+
+const skills = [{
+        name: "Java",
+        value: 90,
+    },
+    {
+        name: "Hibernete",
+        value: 85,
+    },
+    {
+        name: "Springboot",
+        value: 80,
+    },
+    {
+        name: "Javascript",
+        value: 60,
+    },
+    {
+        name: "React.js",
+        value: 50,
+    },
+];
+
+const services = [{
+        title: "Backend Development",
+        description: "Backend with springboot and hibernete and java",
+    },
+    {
+        title: "Web Development",
+        description: "Design web using HTML, CSS, JavaScript and React.js.",
+    },
+    {
+        title: "Database",
+        description: "Backend with database using MySql, Sql, Sql server.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "mdazmatraza53@gmail.com",
+    YOUR_FONE: "(+91)9525253511",
+    description: "I am available for freelance projects.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/AZMATRAZA",
+    facebook: "https://www.facebook.com/profile.php?id=100075588998898",
+    linkedin: "https://www.linkedin.com/in/md-azmat-raza/",
+    twitter: "https://twitter.com/MDAZMATRAZA_?t=Y662utztlq7oIpdRDqP3yA&s=09",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
