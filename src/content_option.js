4086659fc3b5d14afc7304eca88fe4ae2155faeb
Md Azmat Raza <61646903+AZMATRAZA@users.noreply.github.com>
2023-06-29 17:28:47 +0530

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -11,12 +11,24 @@ const introdata = {
         second: "I code cool websites",
         third: "I develop web apps",
     },
-    description: "I’m Md Azamt Raza, a Software developer with 4 years of experience in web development. I have worked with many startups and established companies to build their websites. I love coding and always look for new challenges and opportunities to learn new things.",
+    description: "Hi, I'm Md Azmat Raza, a passionate software developer with experience in designing and developing innovative applications. I hold a Bachelor of Technology in Computer Science from Maulana Azad National Urdu University, where I graduated with a commendable academic record.
+
+Throughout my career, I have gained valuable expertise in various programming languages, including C, C++, Java, and JavaScript. I am skilled in utilizing frameworks like Springboot and Hibernate, as well as developing microservices. My proficiency extends to tools and platforms such as BIT-Bucket, Jira, and GitHub, allowing me to collaborate effectively and deliver high-quality code.
+
+During my time at Foraysoft Pvt Ltd., where I was deployed at TCS, I contributed to the development of Ericsson Network Management (ENM), a comprehensive telecom network management system. I played a key role in releasing ENM to customers every three weeks, ensuring its operational support for 3G, LTE, 4G, and 5G networks.
+
+At Zawsoft Web Solutions, I actively participated in the development of interactive web applications using Java and employed my expertise in system design and architecture. I successfully enhanced the user interface and streamlined the service request process of an internal server request tool.
+
+In addition to my professional experience, I have worked on several projects, including a Hospital Management System implemented using Java and MySQL. This B2B product utilized a SAAS platform and featured separate modules for departments such as Pharmacy, Patient Registration, and CSSD. I also contributed to an event-based project, where I was involved in project setup, database design, and documentation, collaborating closely with project consultants.
+
+Another noteworthy project I worked on was a File Sharing App, which enabled secure file sharing with email authentication and user validation. I was responsible for developing the web application and implementing robust data management using Java and MySQL.
+
+With a passion for continuous learning, I stay updated with the latest industry trends and technologies. I am a self-motivated and detail-oriented individual, dedicated to delivering efficient and reliable software solutions.",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
-    title: "About my Self",
+    title: "About my self: ",
     aboutme: "Have exposure♛ to building professional websites🖥 and front-end applications 💻. Experienced with a demonstrated history of working in the information technology and services industries🏢 . Skilled in FrontEnd Technologies✍️ as React.js, Redux, EcmaScript, JavaScript, HTML, CSS, Bootstrap, Flexbox, .GitHub, Net, SQL, and C# for web and window-based applications. Apart from my professional journey, I am a learner📝 and enthusiastic to learn new tech. I have created 15+ mini & major projects💼 on Reactjs and other technologies.Also deployed and having live-hosted projects📽, checkout out my GitHub pages📒 or YouTube📲 channel for more details. Responsibilities- Developing new user-facing features using React.js Building reusable components and front-end libraries for future use Translating designs into high quality code Optimizing components for maximum performance Optimizing performance of the application.Troubleshooting the bugs Skills- Strong proficiency in JavaScript and the JavaScript object model In-depth understanding of React.js and its core principles Experience with popular React.js workflows (Redux) Experience with React Hooks and building custom react-hooks Familiarity with newer specifications of ECMAScript Knowledge of server-side rendering Familiarity with RESTful APIs Knowledge of Typescript Knowledge of Redux middleware such as Redux Saga & Redux Thunk Good Understanding with common front-end development tools such as Babel, Webpack, NPM, yarn etc.",
 };
 const worktimeline = [{
